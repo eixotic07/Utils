7c2625627a76ed14f69e68998905eef1d227825d
eixotic07 <91800961+eixotic07@users.noreply.github.com>
2023-02-08 15:26:13 -0600

    Create LowestServerTeleport.lua

---
+++ LowestServerTeleport.lua
@@ -0,0 +1,28 @@
+function ServerTeleport()
+    if foundAnything ~= "" then
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
+    else
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+    end
+
+    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+    	foundAnything = Site.nextPageCursor
+    end
+
+    local LowestServerCap = 100
+    for i,v in pairs(Site.data) do
+        local Possible = true 
+        ID = tostring(v.id)
+
+        if v.playing and tonumber(v.maxPlayers) > tonumber(v.playing) then
+            if tonumber(v.playing) <= LowestServerCap then
+                LowestServerCap = tonumber(v.playing)
+                LowestId = ID
+            end
+        end
+    end
+
+	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, LowestId, game.Players.LocalPlayer)
+end
+
+return ServerTeleport
